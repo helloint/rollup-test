0419e14b7ac90a42af7cbceafd696b695aba6b7e
Wayne Mao <wayne.mao@neulion.com.cn>
2021-08-24 16:14:55 +0800

    try 'lodash-es', tree-shaking works fine

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ export {default as PageNav} from './pageNav';
 
 import {version} from '../package.json';
 import answer from 'the-answer';
+import {floor} from 'lodash-es';
 
 export default function () {
 	console.log('version ' + version);
 	console.log(`the answer is ${answer}`);
+	console.log(`floor(4.006): ${floor(4.006)}`);
 }
