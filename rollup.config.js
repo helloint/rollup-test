b947d48281222ec98976ccaf6ef9f5ab3b1fb58b
Wayne Mao <wayne.mao@neulion.com.cn>
2021-08-24 16:07:05 +0800

    try 'rollup-plugin-peer-deps-external'

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,12 @@
 import json from 'rollup-plugin-json';
 import resolve from 'rollup-plugin-node-resolve';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 import pkg from './package.json';
 const inputFileName = 'src/main.js';
 const buildFolder = 'dist/';
-const plugins = [json(), resolve()];
+const plugins = [peerDepsExternal(), json(), resolve()];
 const packageName = pkg.name.replace('@nl/', '');
-
-const isExternalModuleId = (id) => {
-	const ret = /the-answer/.test(id);
-	console.log(`${id}: ${ret}`);
-	return ret;
-};
 
 const cjs = {
 	input: inputFileName,
@@ -20,7 +15,6 @@ const cjs = {
 		format: 'cjs'
 	},
 	plugins: plugins,
-	external: isExternalModuleId
 };
 const esm = {
 	input: inputFileName,
@@ -29,7 +23,6 @@ const esm = {
 		format: 'es'
 	},
 	plugins: plugins,
-	external: isExternalModuleId
 };
 const umd = {
 	input: inputFileName,
@@ -39,7 +32,6 @@ const umd = {
 		name: packageName,
 	},
 	plugins: plugins,
-	external: isExternalModuleId
 };
 
 export default [cjs, esm, umd];
