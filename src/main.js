dcea4457fdf73bee46831059b63eb3157476d83a
Wayne Mao <wayne.mao@neulion.com.cn>
2021-08-24 16:35:21 +0800

    try switch 'lodash', test result. require 'rollup-plugin-commonjs'

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ export {default as PageNav} from './pageNav';
 
 import {version} from '../package.json';
 import answer from 'the-answer';
-import {floor} from 'lodash-es';
+// import {floor} from 'lodash';	// tree-shaking doesn't work
+import floor from 'lodash/floor';	// tree-shaking works, best practice
+// import floor from 'lodash.floor';	// tree-shaking works, but not good to install such many deps
 
 export default function () {
 	console.log('version ' + version);
